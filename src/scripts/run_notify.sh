05cc0850318d1c18cc5a6a9597dc8618546255e1
Kyle a.k.a. TechSquidTV <33272306+KyleTryon@users.noreply.github.com>
2023-10-06 12:14:23 -0400

    fix: trigger correct shell (#26)

---
+++ src/scripts/run_notify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script requires Bash v4+ or zsh.
+# MacOS on CircleCI ships with Bash v3.x as the default shell
+# This script determines which shell to execute the notify script in.
+
+if [[ "$(uname -s)" == "Darwin" && "$SHELL" != "/bin/zsh" ]]; then
+  echo "Running in ZSH on MacOS"
+  /bin/zsh -c "setopt KSH_ARRAYS BASH_REMATCH; $JIRA_SCRIPT_NOTIFY"
+else 
+  /bin/bash -c "$JIRA_SCRIPT_NOTIFY"
+fi
