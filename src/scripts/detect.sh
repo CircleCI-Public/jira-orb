2300217b60fbb69f5f640123b95386c4ed767f22
Jon Carstens <jjcarstens@me.com>
2024-04-01 12:00:15 -0600

    Support using `jira/notify` multiple times with states
    Jira deployments and builds support multiple states to give introspection
    into the deployment flow. Currently, the orb only allows sending `failed`
    or `successful`.
    
    This change allows using `jira/notify` multiple times in order to send other
    states like `pending`, `in_progress`, or `rolled_back` depending on the
    needs to the user workflow

--- src/scripts/detect.sh
+++ src/scripts/detect.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "${JOB_STATUS}" >/tmp/circleci_jira_status
+jira_status=${JIRA_VAL_STATE:-$JOB_STATUS}
+
+if [[ "${JIRA_VAL_JOB_TYPE}" == "build" && "${JIRA_VAL_STATE}" == "rolled_back" ]]; then
+  echo "Cannot use 'rolled_back' build job type. Using '${JOB_STATUS}'"
+  jira_status="${JOB_STATUS}"
+elif [[ "${JIRA_VAL_STATE}" == "unknown" ]]; then
+  jira_status="${JOB_STATUS}"
+fi
+
+echo "${jira_status}" >/tmp/circleci_jira_status
